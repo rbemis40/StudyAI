aa0b7c4d8065dcb78201c291d65acbd71fa762c3
Ryan Bemis <ryanbemis@umass.edu>
2025-11-18 21:18:52 -0500

    More vercel testing with images

--- studyai-app/app/page.tsx
+++ studyai-app/app/page.tsx
@@ -1,9 +1,16 @@
 import styles from "@/app/home.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <div id={styles.test}>Hello world!</div>
+      <Image 
+        src="/wip.jpg" 
+        alt="Work in Progress"
+        width={893}
+        height={360}
+        style={{display: "block", margin: "auto"}}
+      />
     </main>
   );
 }
